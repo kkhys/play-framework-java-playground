6561a3b765e0840ee3f63055b48edd7d490c0ee6
kkhys <kkhys@pm.me>
2024-02-10 18:56:22 +0900

    chore(tailwind): update tailwind css configuration and build scripts
    Implemented Tailwind CSS integration into the project and updated the build scripts in the package.json file to develop and build with Tailwind CSS. The .css and .html files were adjusted accordingly to accommodate the new design framework.

--- frontend/prettier.config.mjs
+++ frontend/prettier.config.mjs
@@ -1,12 +1,16 @@
 /** @typedef {import('prettier').Config} PrettierConfig */
 /** @typedef {import('@ianvs/prettier-plugin-sort-imports').PluginConfig} SortImportsConfig */
+/** @typedef {import('prettier-plugin-tailwindcss').PluginOptions} TailwindConfig */
 
-/** @type {PrettierConfig | SortImportsConfig} */
+/** @type {PrettierConfig | SortImportsConfig | TailwindConfig} */
 const config = {
   jsxSingleQuote: true,
   singleQuote: true,
   trailingComma: 'all',
-  plugins: ['@ianvs/prettier-plugin-sort-imports'],
+  plugins: [
+    '@ianvs/prettier-plugin-sort-imports',
+    'prettier-plugin-tailwindcss',
+  ],
   importOrder: ['<THIRD_PARTY_MODULES>', '', '^#/', '^[../]', '^[./]'],
   importOrderParserPlugins: ['typescript', 'jsx', 'decorators-legacy'],
   importOrderTypeScriptVersion: '4.4.0',
--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,9 +1,9 @@
 import type { Config } from 'tailwindcss';
 
 export default {
-    content: ['../app/views/*.scala.html'],
-    theme: {
-        extend: {},
-    },
-    plugins: [],
+  content: ['../app/views/*.scala.html'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
 } satisfies Config;
